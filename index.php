937220a00d76c18bffb4a728ef74afb4a6fbbe2a
zer0complaints <57272558+zer0complaints@users.noreply.github.com>
2020-04-14 11:40:44 -0700

    Added all files of repl.it portfolio via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Jason Argao Period 6]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">Homepage</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Argao_AboutMe.html">About Me</a></li></br>
+      <li><a href="Argao_Projects.html">Projects</a></li></br>
+      <li><a href="Argao_Resume.html">Resume</a></li></br>
+      <li><a href="Argao_ContactMe.html">Contact Me</a></li></br>
+    <ul>
+  </body>
+</html>
